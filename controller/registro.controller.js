ef8d26a7f2781bc87bb632f5e428dabf16e119dd
Nicolas Martinez <mzadantemartinez@gmail.com>
2022-07-30 12:20:19 -0300

    cree registro.controler con su client service con metodo POST y cree datos nuevos en db.json

---
+++ controller/registro.controller.js
@@ -0,0 +1,22 @@
+import { clientServices } from "../service/client-service.js";
+const formulario = document.querySelector("[data-form]");
+
+formulario.addEventListener("submit", (evento)=>{
+    evento.preventDefault();
+    
+    const url=document.querySelector("[data-url]").value;
+    const name=document.querySelector("[data-nombre]").value;
+    const price=document.querySelector("[data-price]").value;
+    const description=document.querySelector("[data-description]").value;
+
+    //console.log(nombre,"--",url,"--",price,"--",description);
+
+    clientServices.crearProducto(url,name,price,description)
+    .then(respuesta =>{
+        console.log(respuesta);
+    })
+    .catch(err => console.log(err));
+});
+
+
+
--- service/client-service.js
+++ service/client-service.js
@@ -3,6 +3,18 @@
 //creo una funcion de lista de productos resumida con fetch api (por default fetch obtiene get) queda resumida en una linea
 const listaProductos = () => fetch("http://localhost:3000/producto").then( (respuesta) => respuesta.json());
 
+const crearProducto =(url,name,price,description) => {
+    fetch("http://localhost:3000/producto",{
+        method: "POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({url,name,price,description, id: uuid.v4()})
+    })
+
+}
+
 export const clientServices={
     listaProductos,
+    crearProducto,
 };
